6e26334a86731c3ea727472b28c26c23c16b44ba
Ben Warren <warrenbhw@gmail.com>
2026-01-13 12:20:16 -0800

    Add persistent storage support

---
+++ storage.js
@@ -0,0 +1,11 @@
+// Persistent storage for todos
+const STORAGE_KEY = 'todos';
+
+export function saveTodos(todos) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+}
+
+export function loadTodos() {
+  const data = localStorage.getItem(STORAGE_KEY);
+  return data ? JSON.parse(data) : [];
+}
