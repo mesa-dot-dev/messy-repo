2b3b1b6c03efb5305e92e4067dbbe9f55d2a0915
Ben Warren <warrenbhw@gmail.com>
2026-01-13 18:28:43 -0800

    fix auth tests

--- auth.test.js
+++ auth.test.js
@@ -1,7 +1,19 @@
-import { login } from './auth.js';
+import { login, logout, isLoggedIn, getCurrentUser } from './auth.js';
 
 // test login
 function testLogin() {
   login('user', 'pass');
-  // TODO: add assertion
+  console.assert(isLoggedIn() === true, 'should be logged in');
+  console.assert(getCurrentUser().username === 'user', 'username should match');
 }
+
+// test logout
+function testLogout() {
+  login('user', 'pass');
+  logout();
+  console.assert(isLoggedIn() === false, 'should be logged out');
+}
+
+testLogin();
+testLogout();
+console.log('All tests passed!');
